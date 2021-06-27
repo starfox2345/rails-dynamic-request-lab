ba101cb9dcd18ebc815944756f67b69c831e54e7
yatsun <yatsunlam25@gmail.com>
2021-06-26 23:56:18 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,12 @@
+require 'pry'
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+  end
+
+  def show
+    # binding.pry
+    # @students = Student.all
+    @student = Student.find_by_id(params[:id])
   end
 end
